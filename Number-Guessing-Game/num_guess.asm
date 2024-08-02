70f0a92510bf475179399916b7c2e59f618f6d73
Turtel216 <papadimitrios216@gmail.com>
2024-08-02 14:36:39 +0300

    Create guessing game

---
+++ Number-Guessing-Game/num_guess.asm
@@ -0,0 +1,18 @@
+section .text
+  global _start
+
+section .data
+  msg db "Guess The Number!\n", 0ah
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 17
+    syscall
+    mov rax, 60
+    mov rdi, 0
+    syscall
